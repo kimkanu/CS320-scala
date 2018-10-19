c070ab2324d32eeedbb0c09f8ccdabbcdc684ec7
Keonwoo Kim <utophii@kaist.ac.kr>
2018-10-20 01:44:16 +0900

    gitignore

--- BFAE/src/Main.scala
+++ BFAE/src/Main.scala
@@ -15,59 +15,60 @@ object Main extends BFAEImpl {
   def run(str: String): String = {
     def interp(body: BFAE, env: Env, sto: Sto): (BFAEValue, Sto) = body match {
       case Num(num) => (NumV(num), sto)
-      case Add(left, right) => {
+
+      case Add(left, right) =>
         val (lv, ls) = interp(left, env, sto)
         val (rv, rs) = interp(right, env, ls)
         (lv, rv) match {
           case (NumV(l), NumV(r)) => (NumV(l + r), rs)
           case _ => error(s"contains an NaN: $lv, $rv")
         }
-      }
-      case Sub(left, right) => {
+
+      case Sub(left, right) =>
         val (lv, ls) = interp(left, env, sto)
         val (rv, rs) = interp(right, env, ls)
         (lv, rv) match {
           case (NumV(l), NumV(r)) => (NumV(l - r), rs)
           case _ => error(s"contains an NaN: $lv, $rv")
         }
-      }
+
       case Id(name) => (env.getOrElse(name, error(s"free identifier: $name")), sto)
+
       case Fun(id, body) => (CloV(id, body, env), sto)
-      case App(f, a) => {
+
+      case App(f, a) =>
         val (fv, fs) = interp(f, env, sto)
         val (av, as) = interp(a, env, fs)
         fv match {
           case CloV(x, b, fenv) => interp(b, fenv + (x -> av), as)
           case _ => error(s"not a closure: $fv")
         }
-      }
-      case NewBox(e) => {
+        
+      case NewBox(e) =>
         val (v, s) = interp(e, env, sto)
         val addr = malloc(s)
         (BoxV(addr), s + (addr -> v))
-      }
-      case SetBox(b, e) => {
+        
+      case SetBox(b, e) =>
         val (bv, bs) = interp(b, env, sto)
         bv match {
-          case BoxV(addr) => {
+          case BoxV(addr) =>
             val (v, s) = interp(e, env, bs)
             (v, s + (addr -> v))
-          }
           case _ => error(s"not a box: $bv")
         }
-      }
-      case OpenBox(b) => {
+
+      case OpenBox(b) =>
         val (bv, bs) = interp(b, env, sto)
         bv match {
           case BoxV(addr) =>
             (bs.getOrElse(addr, error(s"invalid box: no value at address $addr")), bs)
           case _ => error(s"not a box: $bv")
         }
-      }
-      case Seqn(f, s) => {
+        
+      case Seqn(f, s) =>
         val (fv, fs) = interp(f, env, sto)
         interp(s, env, fs)
-      }
     }
 
     def malloc(sto: Sto): Addr = 1 + sto.keySet.+(0).max
